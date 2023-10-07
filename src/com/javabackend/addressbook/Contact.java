7126720fc647bab30ab35b95b1195744a98a7a27
Akshay444Kumar <aryakshay444@gmail.com>
2023-10-07 22:23:14 +0530

    Added: Contact class

---
+++ src/com/javabackend/addressbook/Contact.java
@@ -0,0 +1,12 @@
+package com.javabackend.addressbook;
+
+public class Contact {
+    String firstName;
+    String lastName;
+    String address;
+    String city;
+    String state;
+    int zipCode;
+    long phoneNo;
+    String email;
+}
